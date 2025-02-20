0cef07395de14e1f08fcc769e796c781b242c821
Crissy Ridden <crissyridden@gmail.com>
2025-02-20 21:50:25 +1100

    commit

--- header.php
+++
@@ -1,9 +0,0 @@
-<?php get_header(); ?>
-
-<div class="content">
-</div>
-
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
-
-
-<?php get_footer(); ?>
