c3713f4c19cd588eb8dc8035bb8b1d8bbe02d2c9
Crissy Ridden <crissyridden@gmail.com>
2025-02-27 08:04:36 +1100

    scroll bar

--- script.js
+++ script.js
@@ -41,6 +41,18 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     });
 
-});
+});window.addEventListener("scroll", function () {
+    let scrollTopBtn = document.getElementById("scrollTopBtn");
+    if (window.scrollY > 300) {
+      scrollTopBtn.style.display = "block";
+    } else {
+      scrollTopBtn.style.display = "none";
+    }
+  });
+  
+  function scrollToTop() {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  }
+  
 
 
