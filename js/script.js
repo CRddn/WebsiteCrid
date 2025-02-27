e03f1a36d33398e77d10eca82fe8d53d58cd054b
Crissy Ridden <crissyridden@gmail.com>
2025-02-27 22:03:32 +1100

    homepage

--- js/script.js
+++ js/script.js
@@ -40,19 +40,4 @@ document.addEventListener("DOMContentLoaded", function () {
             navMenu.classList.remove("active");
         }
     });
-
-});window.addEventListener("scroll", function () {
-    let scrollTopBtn = document.getElementById("scrollTopBtn");
-    if (window.scrollY > 300) {
-      scrollTopBtn.style.display = "block";
-    } else {
-      scrollTopBtn.style.display = "none";
-    }
-  });
-  
-  function scrollToTop() {
-    window.scrollTo({ top: 0, behavior: "smooth" });
-  }
-  
-
-
+})
